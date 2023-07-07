182d3d780094c50676934155d36dc86edf8bf8c3
joseordonez-dev <jose.ordonez@3pillarglobal.com>
2023-07-07 14:35:23 -0600

    Add Table 1

--- state/Tables/dbo.Partners.sql
+++ state/Tables/dbo.Partners.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[Partners]
 (
 [PartnerId] [int] NOT NULL IDENTITY(1, 1),
 [PartnerName] [nvarchar] (30) COLLATE Modern_Spanish_CI_AS NULL,
-[Active] [bit] NULL
+[Active] [bit] NULL,
+[Field1] [nvarchar] (50) COLLATE Modern_Spanish_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Partners] ADD CONSTRAINT [PK_Partners] PRIMARY KEY CLUSTERED ([PartnerId]) ON [PRIMARY]
---
+++ state/Tables/dbo.Table_1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_1]
+(
+[IdTable] [int] NULL,
+[Name] [nchar] (10) COLLATE Modern_Spanish_CI_AS NULL
+) ON [PRIMARY]
+GO
