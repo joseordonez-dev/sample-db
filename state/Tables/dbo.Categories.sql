57b13b6a1af9d535cc676f710785f3dac3552d32
joseordonez-dev <jose.ordonez@3pillarglobal.com>
2023-07-11 12:29:08 -0600

    Add Field to Categories

--- state/Tables/dbo.Categories.sql
+++ state/Tables/dbo.Categories.sql
@@ -3,7 +3,8 @@ CREATE TABLE [dbo].[Categories]
 [CategoryID] [int] NOT NULL IDENTITY(1, 1),
 [CategoryName] [nvarchar] (15) COLLATE Modern_Spanish_CI_AS NOT NULL,
 [Description] [ntext] COLLATE Modern_Spanish_CI_AS NULL,
-[Picture] [image] NULL
+[Picture] [image] NULL,
+[ExtraField] [nvarchar] (50) COLLATE Modern_Spanish_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Categories] ADD CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([CategoryID]) ON [PRIMARY]
