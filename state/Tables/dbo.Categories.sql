f9c300ce2f9b1d80ac4cc9d133edccfea0a0cf32
joseordonez-dev <jose.ordonez@3pillarglobal.com>
2023-07-11 18:31:23 -0600

    Remove ExtraField Column from Categories Table

--- state/Tables/dbo.Categories.sql
+++ state/Tables/dbo.Categories.sql
@@ -3,8 +3,7 @@ CREATE TABLE [dbo].[Categories]
 [CategoryID] [int] NOT NULL IDENTITY(1, 1),
 [CategoryName] [nvarchar] (15) COLLATE Modern_Spanish_CI_AS NOT NULL,
 [Description] [ntext] COLLATE Modern_Spanish_CI_AS NULL,
-[Picture] [image] NULL,
-[ExtraField] [nvarchar] (50) COLLATE Modern_Spanish_CI_AS NULL
+[Picture] [image] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Categories] ADD CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([CategoryID]) ON [PRIMARY]
